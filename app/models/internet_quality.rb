64f092de3feb51058bc710dd98ed6b657432035a
Raghu Betina <raghu@firstdraft.com>
2019-06-06 19:06:56 +0000

    Direct association between internet_qualities and study_location_details

--- app/models/internet_quality.rb
+++ app/models/internet_quality.rb
@@ -1,6 +1,9 @@
 class InternetQuality < ApplicationRecord
   # Direct associations
 
+  has_many   :study_location_details,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- spec/models/study_location_detail_spec.rb
+++ spec/models/study_location_detail_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe StudyLocationDetail, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:internet_quality) }
+
     it { should belong_to(:noise_level) }
 
     it { should belong_to(:ambience) }
