ff894bc95b90b480a2576c3b0c152b279e4f258c
Raghu Betina <raghu@firstdraft.com>
2019-06-06 19:07:33 +0000

    Whitelist columns in InternetQuality for ActiveAdmin

--- app/admin/internet_qualities.rb
+++ app/admin/internet_qualities.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register InternetQuality do
+
+ permit_params :quality
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
