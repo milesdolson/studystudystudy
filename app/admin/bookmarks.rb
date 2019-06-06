2fc89237816468184330ccf94b0b222cd7f9e3b9
Raghu Betina <raghu@firstdraft.com>
2019-06-06 19:07:45 +0000

    Whitelist columns in Bookmark for ActiveAdmin

--- app/admin/bookmarks.rb
+++ app/admin/bookmarks.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Bookmark do
+
+ permit_params :study_location_id, :category_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
