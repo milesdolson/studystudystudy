570e171eb8da69b162dcfc446d10623197a62e0f
Raghu Betina <raghu@firstdraft.com>
2019-06-06 19:08:28 +0000

    Only study_location_detail user can modify study_location_detail

--- app/controllers/study_location_details_controller.rb
+++ app/controllers/study_location_details_controller.rb
@@ -1,4 +1,14 @@
 class StudyLocationDetailsController < ApplicationController
+  before_action :current_user_must_be_study_location_detail_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_study_location_detail_user
+    study_location_detail = StudyLocationDetail.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == study_location_detail.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @study_location_details = StudyLocationDetail.all
 
